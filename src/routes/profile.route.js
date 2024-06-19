0ac02260e50b618f63579efd73098e1d1ef1c7c5
Muhammad Rizky Aulia Gobel <muhrizkyauliagobel@gmail.com>
2024-06-19 08:02:27 +0700

    Update profile.route.js

--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { userId } = require(path.join(__dirname, '../controller/userController.js'));
+const { userId } = require(path.join(__dirname, '../controller/controller.js'));
 const router = express.Router();
 
 router.get('/user/:id', userId);
