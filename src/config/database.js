1b34b9f2bb4a8b8658ac3a896b5581f31b9ce518
Muhammad Rizky Aulia Gobel <muhrizkyauliagobel@gmail.com>
2024-06-21 12:49:06 +0700

    Renewed

--- src/config/database.js
+++ src/config/database.js
@@ -1,20 +1,23 @@
-// database.js
 const Sequelize = require('sequelize');
 require('dotenv').config();
 const mysql2 = require('mysql2');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        host: isProduction ? `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}` : process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
         dialectModule: mysql2,
         define: {
             timestamps: false
-        }
+        },
+        dialectOptions: isProduction ? {
+            socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
+        } : {}
     }
 );
 
